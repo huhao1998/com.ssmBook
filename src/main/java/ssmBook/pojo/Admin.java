49c63443bd7af3ebc48c21cb4f7bbbd21c06750e
Gimmick <gbwjq2010@163.com>
2020-07-09 03:09:45 +0800

    pojo层实现

--- src/main/java/ssmBook/pojo/Admin.java
+++ src/main/java/ssmBook/pojo/Admin.java
@@ -5,4 +5,33 @@ package ssmBook.pojo;
  * @GimmciK 2020.7.8
  */
 public class Admin {
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
